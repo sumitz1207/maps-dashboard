48ca57e00e5e44caf08ca207d206e717a8d0b7dc
Sumit Patel <sumitz1207@gmail.com>
2023-02-12 20:48:14 -0500

    Add files via upload

---
+++ sample_data.js
@@ -0,0 +1,52 @@
+const samplePosts = [
+  {
+    _id: 'abcde',
+    title: 'This is Some Sample Data',
+    author: 'Tae Sung Kim',
+    imageUrl: 'https://source.unsplash.com/1600x900/?corgi',
+    createdAt: '2017-11-14T05:57:26.037Z',
+    body: `Apart from this first post, all of the posts in this set of sample data contain semi-pronouncable random gibberish.
+To generate this gibberish, we stitched together a bunch of paragraphs generated using the Chance.js library. Chance.js will allow you to programatically generate random bits of data, from blog posts to addresses to d20 rolls. You won't actually use Chance.js in this assignment, but we figured you would enjoy knowing just where the very strange words in the remaining sample data came from. 
+We've provided this sample data as an aid to you in the first steps of building the client-side portion of this project. You should eventually replace this data with blog posts obtained from your database.`,
+    views: 214
+  },
+  {
+    _id: 'abcdf',
+    title: 'Use This Data to Help You Build Your Client',
+    author: 'Sandeep Gonnabathula',
+    imageUrl: 'https://source.unsplash.com/1600x900/?mountain,sunset',
+    createdAt: '2017-11-13T03:30:26.037Z',
+    body: `Bul gi addez mic abeahiza tilnor reja petiboca gu tetit wavvahigi mipcuf ben gizen vozin avojagu. For kojir bededa re getbo puf zet gib muvsanej vowpestij vuijdek re wat fatgimamo igo.
+Vapdonfa pin obcizto nounhof for hic ju boz il osegalu lumdurdeb fiw gomnec mi le vel. Popav lakcebobi raj mo uv panore lif ciw ito fud kanirged oza zivom gorec cucsoh fi bomeore ot. Tugkov moupusuj lisne nihcih ne givej rilzafko wucgu mijev hioc to filod peruclus ozug taego. Vo cinoru vef luriz datofgum amuje pavdahih ar vectu luvrem juw riz rubo os le hedwekdic. 
+Ahatim ame za goja oduhewe muptokih okubulbaj be dibup ijakesoze fizjum kuh ufo. Bitga ap make opodovfic gadi fowatsi hin got iv ez aca izila jignugos pajwak relugara acrala niknel. Kuvu fo tad eggop evakaha ihi zigvuw icarod sas he ezeda ut ot napna. Dugetuom og tu ac con cemize zohic ru je icitaedo ze harhu ca. 
+Ron fasgadju acinu tos inulioj huwev zanoc pa gime zir nefsulu fu tonvo. Timgu afovefij weavoek fos be huacu casuvvit da zo kac vo lahi wuvorjif serawnog ob rakbi.`,
+    views: 501
+  },
+  {
+    _id: 'abcdg',
+    title: 'Then Replace It With Data Obtained from your Server and Database',
+    author: 'Sandeep Gonnabathula',
+    imageUrl: 'https://source.unsplash.com/1600x900/?airplane',
+    createdAt: '2017-11-11T01:24:26.037Z',
+    body: `Ebihalar ejo wemes tentewih awziv ocanerir sitbi bod jahfu dumtivub sucov om rapic hasgusbep hogtolfa beccuro usiro nepguup. Co eduuzo cewaddo ganucite zul cedma una wi ejuweto kivud wed ge zi tuw lom wuelupih.
+Gusko roojse efu fa gobtajaj basdiprah wedo jojemko sueliw dun kubbofe dav ne nu. Ji ram uviv hahjishi tum pagoj parti hejru ofitaej mewog ih riki ej nojacati ja catepato. Lueni nowuse vaoz movo dopo nausbu wis ekmezgez wuf kacoco kegcavu len egdamafi car funovfij sagu tamuzif luhmagwi.
+Afjazib ura majiav ofe lu sijov vo supozho hemamice fu heros ca utatif ral gembi kaheh kitpampih. Vupsa ejevoove ledpifki veod sasoped hikuma rilgutu hecku ro ed ot eho. Ku mesgomca asauna sopgannu opuusa monu belem fawi fa du ge lebo noturaoj dekaw piznif fehlemto jugilfa wav. Gak utsomho zut weucco dinwip panoc bafivuca wop fuvi ajaor obe ger ve oga.
+So noejfu sazrik kirno ze hapnuhjaw fejhug ra zeh renfuzpa riv dud. Uri uh onoderi iwalezvu oh wu vov jov cec civni pe hegapa nagal evo kewma fu. Nadi ifoburaz mowi zi cujej marikze fadotus jugec cuigu cifpur usuhud ac jihovav sukorja.
+Pum dotko un huc we zule jakibo celi kefuzkew vepge nefi rogboz firi ezuzuam ba ubecim kibhunrih. Puwoztof si iporo iw holgan el lihkihwe wub pew wihi me tasmu nutvu ra. Fecugeri gulu ceca po et dunubi loituvuh ro ituhu bap gu heuramej.`,
+    views: 318
+  },
+  {
+    _id: 'abcdh',
+    title: 'You Won\'t BELIEVE What Happens Next',
+    author: 'Tae Sung Kim',
+    imageUrl: 'https://source.unsplash.com/1600x900/?guitar',
+    createdAt: '2017-11-08T05:05:26.037Z',
+    body: `Wiebsa we ad jenubsu hamjes pulwa feh zeutgo vumpubsef sos is use fimeh as onbo. Cosgibre bufvinsuv uvohi feg gi uwuam cuvumem fim hufpop uphi na zucarwow of.
+Pasvevet eba lagoes zehbet usu of fid tuirtal zugo keleulo nahihoca rip lovuhew. Ihilus salcisif pumci une naczanigo wupazuki jinuh ebekemvuk vawe jasri olsaturo gufvat itu tumti sorewo gawe wezuwgoh. Ow na ledompot rogfebo ecirehe kujajosuf hosaheb odeur hedpal ovbo jop sun fa ahuhu licwazos rim.
+Fur zeva me wupguwu ari urefaac wamosaz su gippim hezji gefejbo zul firmika ube sajje pu. Kakgi pica meznopfu coj fozlenjo comumre onbi fudem opa wido gad emebe ce nuh vul ici silkajcuv. Jilnagfe vat puub nudus cokasog va zo fusis vuuwu gajminap telpek giwoj nejafev kicuj.
+Ni sechejso gafzobuk ogeni kunov din wevde pedik daun bejawhu jo dovtapsus ha. Bohabe dedfebaf bitug nahina lasij lut tupu tublejje oflofezu fec udajudevo bucruj cibe.`,
+    views: 479
+  }
+];
+
+export default samplePosts;
